0cd02657fabc668d85f330ef3ad84c4341cf4398
v.semenov <bobahansem98@icloud.com>
2024-01-25 12:48:18 +0300

    add png type

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+
+declare module '*.scss' {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
